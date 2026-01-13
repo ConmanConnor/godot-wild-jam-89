1dc5c0908808f02c8aa0180b169936d8fdf133d9
Connor Jones <115162197+ConmanConnor@users.noreply.github.com>
2026-01-13 00:30:50 +0000

    better enemy chase
    -Enemy chases based only on the x axis instead of flying

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,21 +1,29 @@
 extends CharacterBody2D
 @export var ignoreme : CollisionShape2D
 @export var DamageRate : int
-@onready var target = get_node("/root/Node2D/Player")
+@export var Raycast : RayCast2D
+@onready var target #= get_node("/root/Node2D/Player")
+@onready var point = get_node("/root/Node2D/Player")
 var speed = 150
 var enemyColliding : bool 
 
 var Charvelocity = Vector2.ZERO
 
+var faceDire : Vector2
+
 var health = 100
 
 enum{
-	IDLE,
+	PATROL,
 	CHASE,
 	ATTACK,
 	HIT
 }
-var state = CHASE
+var state = PATROL
+
+func _ready() -> void:
+	faceDire = Vector2(-1,-1)
+	scale = faceDire
 
 func TakeDamage(damage: float):
 	health -= damage
@@ -23,32 +31,51 @@ func TakeDamage(damage: float):
 		process_mode = Node.PROCESS_MODE_DISABLED
 		visible = false
 		
+func Patrol(delta):
+	velocity.x = lerp(velocity.x, 0.0 * speed, delta * 5)
+	if Raycast.is_colliding():
+		var collider = Raycast.get_collider()
+		if collider.is_in_group("player"):
+			target = collider
+			state = CHASE
+		else:
+			state = PATROL
+	else:
+		state = PATROL
+		
 func Move(Target,delta):
-	var direction = (Target.position - global_position).normalized()
-	var desired_velocity = direction * speed
-	var steering = (desired_velocity - velocity) * delta * 2.5
-	velocity += steering
-	look_at(target.position)
-	move_and_slide()
+	if Raycast.is_colliding():
+		var collider = Raycast.get_collider()
+		if collider.is_in_group("player"):
+			var dir_x = sign(Target.position.x - global_position.x)
+			velocity.x = lerp(velocity.x, dir_x * speed, delta * 5)
+		else:
+			state = PATROL
+	else:
+		state = PATROL
 
-func _physics_process(delta: float) -> void:
-	#var direction=(target.position - position).normalized()
-	#velocity = direction * speed
-	#look_at(target.position)
-	#move_and_slide()
-	
+func _physics_process(delta: float) -> void:	
+	print (state)
 	match state:
+		PATROL:
+			Patrol(delta)
 		CHASE:
 			Move(target,delta)
 	
 	if enemyColliding:
 		target.TakeDamage(DamageRate * delta)
-
+		
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	#print("Entered:", body.name)
 	if body.is_in_group("player"):
 		print("hit player")
 		enemyColliding = true
-	else:
+		
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("hit player")
 		enemyColliding = false
